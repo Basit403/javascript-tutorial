6f059de458ee19b8eb4ac30073dea4061d4e5bed
Bashiru basit <bashirubasit8@gmail.com>
2022-08-03 23:27:30 +0100

    updated text.js

--- java script tutorial/text2.js
+++ java script tutorial/text2.js
@@ -1,4 +1,28 @@
-var mrVariableshaun;
-mrVariableshaun = 10;
-var myVar2 = "hello";
-myVar2
+var youlikemeat = true;
+
+if (youlikemeat){
+
+    document.write("you like meat!");
+
+} else{
+
+    document.write("you hate meat!");
+
+}
+
+var myage = 49;
+
+if (myage > 30){
+
+    document.write("you are over 30");
+
+} else if(myage > 20){
+    document.write("you are over 20");
+
+} else if(myage > 10){
+    document.write("you are over 10");
+
+} else{
+    document.write("you are not over 10");
+
+}
