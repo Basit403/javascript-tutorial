87f6ef4b80747b016b7f91679efe5d7cc0594753
Bashiru basit <bashirubasit8@gmail.com>
2022-08-08 23:47:58 +0100

    updated text2.js

--- java script tutorial/text2.js
+++ java script tutorial/text2.js
@@ -31,4 +31,12 @@ if (myage > 30){
 
 var links = document.getElementsByTagName("a");
 
+for (i = 1; i <= links.length; i++) {
+
+    console.log("This is link number" + i);
+
+}
+
+document.write("all links are looped");
+
 
